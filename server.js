b2dc077b48f6a379b57816a079c635a84f6e1558
rhodeislandridgeback <92431922+Adamsalvius@users.noreply.github.com>
2022-08-19 13:40:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(process.env.DUMMY_TEXT);
+  res.send(process.env.VAR_SOM_INTE_FINNS);
 });
-
 app.listen(PORT, () => {
   console.log(`haha yes, the server is running on port ${PORT}`);
 });
